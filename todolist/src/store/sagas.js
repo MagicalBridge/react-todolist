a0da147c5b5d1296ed05e3653e9efed25215307f
褚鹏飞 <540788769@qq.com>
2018-12-16 16:55:31 +0800

    commit

--- todolist/src/store/sagas.js
+++ todolist/src/store/sagas.js
@@ -1,14 +1,13 @@
 import { put, takeEvery } from 'redux-saga/effects'
 import axios from 'axios';
 import { GET_INIT_LIST } from './actionTypes';
-import { initListAction } from './actionCreators';
+import { initListAction } from './actionCreators'; 
 
 
 function* getInitList() {
   try {
-    const url = 'http://www.ifruit.org:7300/mock/59afdddae4205b01cfc9ede5/api_1504685109840/front1/login.do'
-    const res = yield axios.post(url, { number: "13127733713" });
-    const action = initListAction(res.data.list);
+    const res = yield axios.get('/list.json');
+    const action = initListAction(res.data);
     yield put(action);
   } catch (e) {
     console.log("网络请求失败")
