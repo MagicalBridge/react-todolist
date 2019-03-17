4ac064d0fa225d8197d5faaae1b2d633d7c03a67
褚鹏飞 <540788769@qq.com>
2019-03-17 10:50:35 +0800

    添加相关的文件

--- todolist/src/common/header/store/index.js
+++
@@ -1,5 +0,0 @@
-import headerReducer from './headerReducer.js';
-import * as actionCreators from './actionCreator.js';
-import * as actionTypes from './actionType.js';
-
-export { headerReducer, actionCreators, actionTypes };
--- todolist/src/store/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import { headerReducer } from '../common/header/store/index.js';
-
-
-export default combineReducers({
-  header: headerReducer
-})
-
-
