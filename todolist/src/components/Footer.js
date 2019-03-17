670d439da4116dba62ec7657db3d45b1a99a3928
褚鹏飞 <540788769@qq.com>
2019-03-17 13:39:58 +0800

    commit 设计 state  完成 toggle 的操作

--- todolist/src/components/Footer.js
+++ todolist/src/components/Footer.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
+    const { filter, setVisibilityFilter } = this.props;
     return (
       <div>
-        
+        <span>show:</span>
+        <button
+          onClick={() => setVisibilityFilter("all")}
+          disabled={ filter === "all" }
+        >
+          all
+        </button>
+        <button
+          onClick={() => setVisibilityFilter("active")}
+          disabled={filter === "active"}
+        >
+          active
+        </button>
+
+        <button
+          onClick={() => setVisibilityFilter("completed")}
+          disabled={filter === "completed"}
+        >
+          completed
+        </button>
       </div>
     );
   }
--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Todo from './Todo';
 
 class TodoList extends Component {
   render() {
+    // 这是一种结构赋值的写法
+    const { todos,toggle } = this.props;
     return (
-      <div>
-        
-      </div>
+      <ul>
+        {todos.map(todo => {
+          return <Todo key={todo.id} {...todo} onClick={()=>{toggle(todo.id)}} />
+        })}
+      </ul>
     );
   }
 }
