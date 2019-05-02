5e5991721b7d130fd74168af0dc569123241c06c
褚鹏飞 <540788769@qq.com>
2019-05-02 21:37:34 +0800

    提交代码

--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -3,8 +3,9 @@ import Todo from './Todo';
 
 class TodoList extends Component {
   render() {
-    // 这是一种结构赋值的写法
-    const { todos,toggle } = this.props;
+    // 这是一种结构赋值的写法 在 App 组件中传入了两个参数 
+    // 一个是 todos 一个toggle 都会挂载在 this.props 这个对象上面 
+    const { todos, toggle } = this.props;
     return (
       <ul>
         {todos.map(todo => {
