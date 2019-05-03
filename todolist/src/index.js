f31c7129224271d4f414a07f340aeed46312ae49
褚鹏飞 <540788769@qq.com>
2019-05-03 16:22:56 +0800

    使用ducks进行数据管理

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunkMiddleware from "redux-thunk";
-import rootReducer from "./reducers";
-import App from "./components/App";
-
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+import App from "./containers/App";
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
